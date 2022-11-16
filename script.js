5431330c2147aa4b42bc5a62e9f1b55224a8ec7a
Aaxtrox <pawelw@gmx.com>
2022-11-16 11:35:36 +0000

    querySelector

---
+++ script.js
@@ -0,0 +1,14 @@
+//grab input   
+const input = document.querySelector('input');
+//grab colorPicker button
+const colorPicker = document.querySelector('#colorPicker');
+//grab Pick Color button
+const pickColor = document.querySelector('#pickColor');
+//grab random button
+const random = document.querySelector('#random');
+//grab eraser button
+const eraserButton = document.querySelector('#eraser');
+//grab clear button
+const clearButton = document.querySelector('#clear');
+//grab div with id of "container"
+let container = document.getElementById("container");
